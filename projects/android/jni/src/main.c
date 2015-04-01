b1340d7cc1dcc8d6c0c451b4b372265b926f4ce7
Joseba García Etxebarria <joseba.gar@gmail.com>
2015-04-01 22:22:28 +0200

    * Removed another compiler warning * Added Android main.c to the Qt Creator project

--- projects/android/jni/src/main.c
+++ projects/android/jni/src/main.c
@@ -1,6 +1,6 @@
 /*
- *  Copyright © 2006-2011 SplinterGU (Fenix/Bennugd)
- *  Copyright © 2002-2006 Fenix Team (Fenix)
+ *  Copyright (C) 2006-2011 SplinterGU (Fenix/Bennugd)
+ *  Copyright (C) 2002-2006 Fenix Team (Fenix)
  *
  *  This file is part of Bennu - Game Development
  *
@@ -32,7 +32,8 @@
 #include "SDL.h"
 #include <stdio.h>
 
-#include "bgdrtm.h"
+#include <bgdrtm.h>
+#include <xstrings.h>
 
 /* ---------------------------------------------------------------------- */
 
@@ -56,82 +57,78 @@ static int embedded    = 0;  /* 1 only if this is a stub with an embedded DCB */
  *
  */
 
-int main( int argc, char *argv[] )
-{
+int main( int argc, char *argv[] ) {
     char * filename = NULL, dcbname[ __MAX_PATH ], *ptr ;
     int i, j, ret = -1;
     file * fp = NULL;
     INSTANCE * mainproc_running;
     dcb_signature dcb_signature;
-    
+
     SDL_Log ("BennuGD init\n");
-    
+
     filename = "main.dcb";
-	if(file_exists("main.dcb"))
+    if(file_exists("main.dcb")) {
         SDL_Log("main.dcb exists in APK\n");
-    else {
+    } else {
         SDL_Log("main.dcb doesn't exist in APK, quitting\n");
         return 1;
     }
-    
+
     // Remember to compile DCB with debug (bgdc -g) info!
     debug = 1;
-	
+
     /* Initialization (modules needed before dcb_load) */
-	
+
     string_init() ;
     init_c_type() ;
-	
+
     /* Init application title for windowed modes */
-	
+
     strcpy( dcbname, filename ) ;
-    
+
     SDL_Log("Loading main.dcb...\n");
-	
+
     /* First try to load directly (we expect myfile.dcb) */
-    if ( !dcb_load( dcbname ) )
-    {
+    if ( !dcb_load( dcbname ) ) {
         char ** dcbext = dcb_exts;
         int dcbloaded = 0;
-        
-        while ( dcbext && *dcbext )
-        {
+
+        while ( dcbext && *dcbext ) {
             strcpy( dcbname, filename ) ;
             strcat( dcbname, *dcbext ) ;
             if (( dcbloaded = dcb_load( dcbname ) ) ) break;
             dcbext++;
         }
-        
-        if ( !dcbloaded )
-        {
+
+        if ( !dcbloaded ) {
             SDL_Log( "%s: doesn't exist or isn't version %d DCB compatible\n", filename, DCB_VERSION >> 8 ) ;
             return -1 ;
         }
     }
-	
+
     /* If the dcb is not in debug mode */
-	
+
     if ( dcb.data.NSourceFiles == 0 ) debug = 0;
-	
+
     /* Initialization (modules needed after dcb_load) */
-	
+
     sysproc_init() ;
-	
+
     argv[0] = filename;
     bgdrtm_entry( argc, argv );
-	
+
     if ( mainproc )
     {
         mainproc_running = instance_new( mainproc, NULL ) ;
         ret = instance_go_all() ;
     }
-	
+
     bgdrtm_exit( ret );
-	
+
     free( appexename        );
     free( appexepath        );
     free( appexefullpath    );
     free( appname           );
-	
+
     return ret;
 }
