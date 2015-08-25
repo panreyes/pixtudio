d0c6e39a7962e45645b3c9c973fc2163a0bddd1f
Joseba García Echebarria <joseba.garcia@mathlas.com>
2015-08-25 11:49:23 +0200

    I should really not be testing for platform, but for the availability of a constant

--- core/bgdrtm/src/dirs.c
+++ core/bgdrtm/src/dirs.c
@@ -266,10 +266,10 @@ __DIR_ST * dir_open( const char * path )
     /* Convert '*.*' to '*' */
     if ( fptr > hDir->pattern + 2 && fptr[ -1 ] == '*' && fptr[ -2 ] == '.' && fptr[ -3 ] == '*' ) fptr[ -2 ] = 0;
 
-#if defined(__APPLE__) || defined(TARGET_BEOS) || defined(TARGET_WII) || defined(TARGET_PSP) || defined(__ANDROID__)
-    r = glob( hDir->pattern, GLOB_ERR | GLOB_NOSORT, NULL, &hDir->globd );
-#else
+#if defined(GLOB_PERIOD)
     r = glob( hDir->pattern, GLOB_ERR | GLOB_PERIOD | GLOB_NOSORT, NULL, &hDir->globd );
+#else
+    r = glob( hDir->pattern, GLOB_ERR | GLOB_NOSORT, NULL, &hDir->globd );
 #endif
 
     if ( r )
