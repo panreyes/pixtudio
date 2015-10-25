74b0e1f18a5346ec728ddfb727a75a501dc30ac8
Joseba García Etxebarria <joseba.gar@gmail.com>
2015-10-25 18:13:53 +0100

    * Changes to compile with OpenAL on OS X using the system framework

--- modules/mod_theora/mod_theora.c
+++ modules/mod_theora/mod_theora.c
@@ -32,8 +32,13 @@
 #include <assert.h>
 
 #include <theoraplay.h>
+#ifdef __APPLE__
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#else
 #include <AL/al.h>
 #include <AL/alc.h>
+#endif
 #include <SDL.h>
 
 /* PixTudio stuff */
