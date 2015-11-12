8d5fcb76a000b77624e97c86a0ff66697cca0989
Joseba García Echebarria <joseba@MacBook-Air-de-Pilar.local>
2015-11-12 20:51:28 +0100

    * Work in adapting mod_regex to TRE library

--- examples/main_tre.c
+++ examples/main_tre.c
@@ -28,7 +28,34 @@ int regex(char *reg, char* str) {
     }
 }
 
+int split(char *reg, char *str) {
+    int result=1;
+    
+    unsigned n;
+    
+    regex_t pb;
+    regmatch_t pmatch[16];
+    
+    /* Match the regex */
+    
+    if (tre_regcomp(&pb, reg, REG_EXTENDED | REG_ICASE) == REG_OK) {
+        result = tre_regexec(&pb, str, 16, pmatch, 0);
+        tre_regfree(&pb);
+        for(n=0; n<16 && pmatch[n].rm_so != -1; n++) {
+            printf("%d: %d<->%d\n", n, pmatch[n].rm_so, pmatch[n].rm_eo);
+            // Store the strings for BennuGD use
+        }
+    }
+    
+    if(result == 0) {
+        return pmatch[0].rm_so;
+    } else {
+        return -1;
+    }
+}
+
 int main(int argc, char *argv[]) {
+    // Test for regex function
     char *str = "It's raining cat's and dogs";
     char *reg = "cat";
 
@@ -36,5 +63,10 @@ int main(int argc, char *argv[]) {
     printf("%d is the last character position in 99 bottles of "
            "beer on the wall\n",
            regex("$","99 bottles of beer on the wall."));
+    
+    // Test for split function
+    str = "A,B,C,D,E";
+    split(",", str);
+    
     return 0;
 }
--- modules/mod_regex/mod_regex.c
+++ modules/mod_regex/mod_regex.c
@@ -39,7 +39,7 @@
 #include "xstrings.h"
 
 #include "dlvaracc.h"
-#include "regex.h"
+#include <tre/tre.h>
 
 /* ----------------------------------------------------------------- */
 
@@ -77,49 +77,35 @@ static int modregex_regex (INSTANCE * my, int * params)
     const char * str = string_get(params[1]);
     int result = -1;
     unsigned n;
+    
+    regex_t pb;
+    regmatch_t pmatch[16];
 
-    struct re_pattern_buffer pb;
-    struct re_registers re;
-    int start[16];
-    int end[16];
     int * regex_reg;
-
-    /* Alloc the pattern resources */
-
-    memset (&pb, 0, sizeof(pb));
-    memset (&re, 0, sizeof(re));
-    pb.buffer = malloc(4096);
-    pb.allocated = 4096;
-    pb.fastmap = malloc(256);
-    pb.regs_allocated = 16;
-    re.num_regs = 16;
-    re.start = start;
-    re.end = end;
-
-    re_syntax_options = RE_SYNTAX_POSIX_MINIMAL_EXTENDED | REG_ICASE;
-
-    /* Match the regex */
-
-    if (re_compile_pattern (reg, strlen(reg), &pb) == 0)
-    {
-        result = re_search (&pb, str, strlen(str), 0, strlen(str), &re);
-
-        if (result != -1)
-        {
-            /* Fill the regex_reg global variables */
+    
+    // Compile the regular expression, then match it
+    if (tre_regcomp(&pb, reg, REG_EXTENDED | REG_ICASE) == REG_OK) {
+        result = tre_regexec(&pb, str, 16, pmatch, 0);
+        tre_regfree(&pb);
+        if (result == 0) {
+            // Fill the regex_reg global variables
             regex_reg = (int *) &GLODWORD( mod_regex, REGEX_REG);
-            for (n = 0 ; n < 16 && n <= pb.re_nsub ; n++)
-            {
+            // Store the strings for PixTudio use
+            for(n=0; n<16 && pmatch[n].rm_so != -1; n++) {
                 string_discard (regex_reg[n]);
-                regex_reg[n] = string_newa (str + re.start[n], re.end[n] - re.start[n]);
+                regex_reg[n] = string_newa (str + pmatch[n].rm_so, pmatch[n].rm_eo - pmatch[n].rm_so);
                 string_use (regex_reg[n]);
             }
         }
     }
+    
+    if(result == 0) {
+        return pmatch[0].rm_so;
+    } else {
+        return -1;
+    }
 
     /* Free the resources */
-    free (pb.buffer);
-    free (pb.fastmap);
     string_discard(params[0]);
     string_discard(params[1]);
 
@@ -136,171 +122,172 @@ static int modregex_regex (INSTANCE * my, int * params)
 
 static int modregex_regex_replace (INSTANCE * my, int * params)
 {
-    const char * reg = string_get(params[0]);
-    const char * rep = string_get(params[1]);
-    const char * str = string_get(params[2]);
-
-    unsigned reg_len = strlen(reg);
-    unsigned str_len = strlen(str);
-    unsigned rep_len = strlen(rep);
-    char * replacement;
-    unsigned replacement_len;
-    int fixed_replacement = strchr(rep, '\\') ? 0:1;
-
-    struct re_pattern_buffer pb;
-    struct re_registers re;
-    int start[16];
-    int end[16];
-
-    unsigned startpos = 0;
-    unsigned nextpos;
-    int regex_filled = 0;
-
-    char * result = 0;
-    unsigned result_allocated = 0;
-    int result_string = 0;
-
-    unsigned n;
-    int * regex_reg;
-
-    /* Alloc a buffer for the resulting string */
-
-    result = malloc(128);
-    result_allocated = 128;
-    *result = 0;
-
-    /* Alloc the pattern resources */
-
-    memset (&pb, 0, sizeof(pb));
-    memset (&re, 0, sizeof(re));
-    pb.buffer = malloc(4096);
-    pb.allocated = 4096;
-    pb.used = 0;
-    pb.fastmap = malloc(256);
-    pb.translate = NULL;
-    pb.fastmap_accurate = 0;
-    pb.regs_allocated = 16;
-    re.start = start;
-    re.end = end;
-
-    re_syntax_options = RE_SYNTAX_POSIX_MINIMAL_EXTENDED;
-
-    /* Run the regex */
-
-    if (re_compile_pattern (reg, reg_len, &pb) == 0)
-    {
-        startpos = 0;
-
-        while (startpos < str_len)
-        {
-            nextpos = re_search (&pb, str, str_len, startpos,
-                str_len - startpos, &re);
-            if ((int)nextpos < 0) break;
-
-            /* Fill the REGEX_REG global variables */
-
-            if (regex_filled == 0)
-            {
-                regex_filled = 1;
-                regex_reg = (int *)&GLODWORD( mod_regex, REGEX_REG);
-                for (n = 0 ; n < 16 && n <= pb.re_nsub ; n++)
-                {
-                    string_discard (regex_reg[n]);
-                    regex_reg[n] = string_newa (str + re.start[n], re.end[n] - re.start[n]);
-                    string_use (regex_reg[n]);
-                }
-            }
-
-            /* Prepare the replacement string */
-
-            if (fixed_replacement == 0)
-            {
-                int total_length = rep_len;
-                const char * bptr;
-                char *  ptr;
-
-                /* Count the size */
-
-                ptr = strchr(rep, '\\');
-                while (ptr)
-                {
-                    if (ptr[1] >= '0' && ptr[1] <= '9')
-                        total_length += re.end[ptr[1]-'0'] - re.start[ptr[1]-'0'] - 2;
-                    ptr = strchr(ptr+1, '\\');
-                }
-
-                /* Fill the replacement string */
-
-                replacement = calloc (total_length+1, 1);
-
-                bptr = rep;
-                ptr = strchr(rep, '\\');
-                while (ptr)
-                {
-                    if (ptr[1] >= '0' && ptr[1] <= '9')
-                    {
-                        strncpy (replacement+strlen(replacement), bptr, ptr-bptr);
-                        strncpy (replacement+strlen(replacement), str + re.start[ptr[1]-'0'], re.end[ptr[1]-'0'] - re.start[ptr[1]-'0']);
-                        bptr = ptr+2;
-                    }
-                    ptr = strchr (ptr+1, '\\');
-                }
-                strcat (replacement, bptr);
-                replacement_len = strlen(replacement);
-            }
-            else
-            {
-                replacement = (char *)rep;
-                replacement_len = rep_len;
-            }
-
-            /* Fill the resulting string */
-
-            if (result_allocated < strlen(result)+(nextpos-startpos)+1+replacement_len)
-            {
-                result_allocated += ((nextpos-startpos+1+replacement_len) & ~127) + 128;
-                result = realloc(result, result_allocated);
-            }
-            result[strlen(result)+(nextpos-startpos)] = 0;
-            memcpy (result + strlen(result), str+startpos, nextpos-startpos);
-            strcat (result, replacement);
-
-            if (fixed_replacement == 0) free (replacement);
-
-            /* Continue the search */
-
-            startpos = nextpos+re_match(&pb, str, str_len, nextpos, 0);
-            if (startpos <  nextpos) break;
-            if (startpos == nextpos) startpos++;
-        }
-    }
-
-    /* Copy remaining characters */
-
-    nextpos = str_len;
-    if (result_allocated < strlen(result)+(nextpos-startpos)+1)
-    {
-        result_allocated += ((nextpos-startpos+1) & ~127) + 128;
-        result = realloc(result, result_allocated);
-    }
-    result[strlen(result)+(nextpos-startpos)] = 0;
-    memcpy (result + strlen(result), str+startpos, nextpos-startpos);
-
-    /* Free resources */
-
-    free (pb.buffer);
-    free (pb.fastmap);
-    string_discard(params[0]);
-    string_discard(params[1]);
-    string_discard(params[2]);
-
-    /* Return the new string */
-
-    result_string = string_new(result);
-    string_use(result_string);
-    free(result);
-
-    return result_string;
+    return 0;
+//    const char * reg = string_get(params[0]);
+//    const char * rep = string_get(params[1]);
+//    const char * str = string_get(params[2]);
+//
+//    unsigned reg_len = strlen(reg);
+//    unsigned str_len = strlen(str);
+//    unsigned rep_len = strlen(rep);
+//    char * replacement;
+//    unsigned replacement_len;
+//    int fixed_replacement = strchr(rep, '\\') ? 0:1;
+//
+//    struct re_pattern_buffer pb;
+//    struct re_registers re;
+//    int start[16];
+//    int end[16];
+//
+//    unsigned startpos = 0;
+//    unsigned nextpos;
+//    int regex_filled = 0;
+//
+//    char * result = 0;
+//    unsigned result_allocated = 0;
+//    int result_string = 0;
+//
+//    unsigned n;
+//    int * regex_reg;
+//
+//    /* Alloc a buffer for the resulting string */
+//
+//    result = malloc(128);
+//    result_allocated = 128;
+//    *result = 0;
+//
+//    /* Alloc the pattern resources */
+//
+//    memset (&pb, 0, sizeof(pb));
+//    memset (&re, 0, sizeof(re));
+//    pb.buffer = malloc(4096);
+//    pb.allocated = 4096;
+//    pb.used = 0;
+//    pb.fastmap = malloc(256);
+//    pb.translate = NULL;
+//    pb.fastmap_accurate = 0;
+//    pb.regs_allocated = 16;
+//    re.start = start;
+//    re.end = end;
+//
+//    re_syntax_options = RE_SYNTAX_POSIX_MINIMAL_EXTENDED;
+//
+//    /* Run the regex */
+//
+//    if (re_compile_pattern (reg, reg_len, &pb) == 0)
+//    {
+//        startpos = 0;
+//
+//        while (startpos < str_len)
+//        {
+//            nextpos = re_search (&pb, str, str_len, startpos,
+//                str_len - startpos, &re);
+//            if ((int)nextpos < 0) break;
+//
+//            /* Fill the REGEX_REG global variables */
+//
+//            if (regex_filled == 0)
+//            {
+//                regex_filled = 1;
+//                regex_reg = (int *)&GLODWORD( mod_regex, REGEX_REG);
+//                for (n = 0 ; n < 16 && n <= pb.re_nsub ; n++)
+//                {
+//                    string_discard (regex_reg[n]);
+//                    regex_reg[n] = string_newa (str + re.start[n], re.end[n] - re.start[n]);
+//                    string_use (regex_reg[n]);
+//                }
+//            }
+//
+//            /* Prepare the replacement string */
+//
+//            if (fixed_replacement == 0)
+//            {
+//                int total_length = rep_len;
+//                const char * bptr;
+//                char *  ptr;
+//
+//                /* Count the size */
+//
+//                ptr = strchr(rep, '\\');
+//                while (ptr)
+//                {
+//                    if (ptr[1] >= '0' && ptr[1] <= '9')
+//                        total_length += re.end[ptr[1]-'0'] - re.start[ptr[1]-'0'] - 2;
+//                    ptr = strchr(ptr+1, '\\');
+//                }
+//
+//                /* Fill the replacement string */
+//
+//                replacement = calloc (total_length+1, 1);
+//
+//                bptr = rep;
+//                ptr = strchr(rep, '\\');
+//                while (ptr)
+//                {
+//                    if (ptr[1] >= '0' && ptr[1] <= '9')
+//                    {
+//                        strncpy (replacement+strlen(replacement), bptr, ptr-bptr);
+//                        strncpy (replacement+strlen(replacement), str + re.start[ptr[1]-'0'], re.end[ptr[1]-'0'] - re.start[ptr[1]-'0']);
+//                        bptr = ptr+2;
+//                    }
+//                    ptr = strchr (ptr+1, '\\');
+//                }
+//                strcat (replacement, bptr);
+//                replacement_len = strlen(replacement);
+//            }
+//            else
+//            {
+//                replacement = (char *)rep;
+//                replacement_len = rep_len;
+//            }
+//
+//            /* Fill the resulting string */
+//
+//            if (result_allocated < strlen(result)+(nextpos-startpos)+1+replacement_len)
+//            {
+//                result_allocated += ((nextpos-startpos+1+replacement_len) & ~127) + 128;
+//                result = realloc(result, result_allocated);
+//            }
+//            result[strlen(result)+(nextpos-startpos)] = 0;
+//            memcpy (result + strlen(result), str+startpos, nextpos-startpos);
+//            strcat (result, replacement);
+//
+//            if (fixed_replacement == 0) free (replacement);
+//
+//            /* Continue the search */
+//
+//            startpos = nextpos+re_match(&pb, str, str_len, nextpos, 0);
+//            if (startpos <  nextpos) break;
+//            if (startpos == nextpos) startpos++;
+//        }
+//    }
+//
+//    /* Copy remaining characters */
+//
+//    nextpos = str_len;
+//    if (result_allocated < strlen(result)+(nextpos-startpos)+1)
+//    {
+//        result_allocated += ((nextpos-startpos+1) & ~127) + 128;
+//        result = realloc(result, result_allocated);
+//    }
+//    result[strlen(result)+(nextpos-startpos)] = 0;
+//    memcpy (result + strlen(result), str+startpos, nextpos-startpos);
+//
+//    /* Free resources */
+//
+//    free (pb.buffer);
+//    free (pb.fastmap);
+//    string_discard(params[0]);
+//    string_discard(params[1]);
+//    string_discard(params[2]);
+//
+//    /* Return the new string */
+//
+//    result_string = string_new(result);
+//    string_use(result_string);
+//    free(result);
+//
+//    return result_string;
 }
 
 /** SPLIT (STRING regex, STRING string, STRING POINTER array, INT array_size)
@@ -312,65 +299,66 @@ static int modregex_regex_replace (INSTANCE * my, int * params)
 
 static int modregex_split (INSTANCE * my, int * params)
 {
-    const char * reg = string_get(params[0]);
-    const char * str = string_get(params[1]);
-    int * result_array = (int *)params[2];
-    int result_array_size = params[3];
-    int count = 0;
-    int pos, lastpos = 0;
-
-    struct re_pattern_buffer pb;
-    struct re_registers re;
-    int start[16];
-    int end[16];
-
-    /* Alloc the pattern resources */
-
-    memset (&pb, 0, sizeof(pb));
-    memset (&re, 0, sizeof(re));
-    pb.buffer = malloc(4096);
-    pb.allocated = 4096;
-    pb.fastmap = malloc(256);
-    pb.regs_allocated = 16;
-    re.num_regs = 16;
-    re.start = start;
-    re.end = end;
-
-    re_syntax_options = RE_SYNTAX_POSIX_MINIMAL_EXTENDED;
-
-    /* Match the regex */
-
-    if (re_compile_pattern (reg, strlen(reg), &pb) == 0)
-    {
-        for (;;)
-        {
-            pos = re_search (&pb, str, strlen(str), lastpos, strlen(str), &re);
-            if (pos == -1) break;
-            *result_array = string_newa (str + lastpos, pos-lastpos);
-            string_use(*result_array);
-            result_array++;
-            count++;
-            result_array_size--;
-            if (result_array_size == 0) break;
-            lastpos = pos + re_match (&pb, str, strlen(str), pos, 0);
-            if (lastpos < pos) break;
-            if (lastpos == pos) lastpos++;
-        }
-        if (result_array_size > 0)
-        {
-            *result_array = string_new (str + lastpos);
-            string_use (*result_array);
-            count++;
-        }
-    }
-
-    /* Free the resources */
-    free (pb.buffer);
-    free (pb.fastmap);
-    string_discard(params[0]);
-    string_discard(params[1]);
-
-    return count;
+    return 0;
+//    const char * reg = string_get(params[0]);
+//    const char * str = string_get(params[1]);
+//    int * result_array = (int *)params[2];
+//    int result_array_size = params[3];
+//    int count = 0;
+//    int pos, lastpos = 0;
+//
+//    struct re_pattern_buffer pb;
+//    struct re_registers re;
+//    int start[16];
+//    int end[16];
+//
+//    /* Alloc the pattern resources */
+//
+//    memset (&pb, 0, sizeof(pb));
+//    memset (&re, 0, sizeof(re));
+//    pb.buffer = malloc(4096);
+//    pb.allocated = 4096;
+//    pb.fastmap = malloc(256);
+//    pb.regs_allocated = 16;
+//    re.num_regs = 16;
+//    re.start = start;
+//    re.end = end;
+//
+//    re_syntax_options = RE_SYNTAX_POSIX_MINIMAL_EXTENDED;
+//
+//    /* Match the regex */
+//
+//    if (re_compile_pattern (reg, strlen(reg), &pb) == 0)
+//    {
+//        for (;;)
+//        {
+//            pos = re_search (&pb, str, strlen(str), lastpos, strlen(str), &re);
+//            if (pos == -1) break;
+//            *result_array = string_newa (str + lastpos, pos-lastpos);
+//            string_use(*result_array);
+//            result_array++;
+//            count++;
+//            result_array_size--;
+//            if (result_array_size == 0) break;
+//            lastpos = pos + re_match (&pb, str, strlen(str), pos, 0);
+//            if (lastpos < pos) break;
+//            if (lastpos == pos) lastpos++;
+//        }
+//        if (result_array_size > 0)
+//        {
+//            *result_array = string_new (str + lastpos);
+//            string_use (*result_array);
+//            count++;
+//        }
+//    }
+//
+//    /* Free the resources */
+//    free (pb.buffer);
+//    free (pb.fastmap);
+//    string_discard(params[0]);
+//    string_discard(params[1]);
+//
+//    return count;
 }
 
 /** JOIN (STRING separator, STRING POINTER array, INT array_size)
@@ -380,40 +368,41 @@ static int modregex_split (INSTANCE * my, int * params)
 
 static int modregex_join (INSTANCE * my, int * params)
 {
-    const char * sep = string_get(params[0]);
-    int * string_array = (int *)params[1];
-    int count = params[2] ;
-    int total_length = 0;
-    int sep_len = strlen(sep);
-    int n;
-    char * buffer;
-    char * ptr;
-    int result;
-
-    for (n = 0 ; n < count ; n++)
-    {
-        total_length += strlen(string_get(string_array[n]));
-        if (n < count-1) total_length += sep_len;
-    }
-
-    buffer = malloc(total_length+1);
-    ptr = buffer;
-
-    for (n = 0 ; n < count ; n++)
-    {
-        memcpy (ptr, string_get(string_array[n]), strlen(string_get(string_array[n])));
-        ptr += strlen(string_get(string_array[n]));
-        if (n < count-1)
-        {
-            memcpy (ptr, sep, sep_len);
-            ptr += sep_len;
-        }
-    }
-    *ptr = 0;
-    result = string_new(buffer);
-    free(buffer);
-    string_use(result);
-    return result;
+    return 0;
+//    const char * sep = string_get(params[0]);
+//    int * string_array = (int *)params[1];
+//    int count = params[2] ;
+//    int total_length = 0;
+//    int sep_len = strlen(sep);
+//    int n;
+//    char * buffer;
+//    char * ptr;
+//    int result;
+//
+//    for (n = 0 ; n < count ; n++)
+//    {
+//        total_length += strlen(string_get(string_array[n]));
+//        if (n < count-1) total_length += sep_len;
+//    }
+//
+//    buffer = malloc(total_length+1);
+//    ptr = buffer;
+//
+//    for (n = 0 ; n < count ; n++)
+//    {
+//        memcpy (ptr, string_get(string_array[n]), strlen(string_get(string_array[n])));
+//        ptr += strlen(string_get(string_array[n]));
+//        if (n < count-1)
+//        {
+//            memcpy (ptr, sep, sep_len);
+//            ptr += sep_len;
+//        }
+//    }
+//    *ptr = 0;
+//    result = string_new(buffer);
+//    free(buffer);
+//    string_use(result);
+//    return result;
 }
 
 /* ---------------------------------------------------------------------- */
