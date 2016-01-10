35f3ec26519f05cc1341e2563044ab49d8d1dfa3
Joseba García Etxebarria <joseba.gar@gmail.com>
2016-01-10 17:56:58 +0100

    Also reformat module code, but not the symbol tables.

--- goodies/reformat_code.sh
+++ goodies/reformat_code.sh
@@ -3,5 +3,10 @@ cd ..
 
 cd core
 for i in $(find -iname "*.[c,h]" | grep -v "monolithic_includes.h"); do
-	clang-format -i -style=file $i;
+    clang-format -i -style=file $i;
+done
+
+cd ../modules
+for i in $(find -iname "*.[c,h]" | grep -v "_symbols.h"); do
+    clang-format -i -style=file $i;
 done
