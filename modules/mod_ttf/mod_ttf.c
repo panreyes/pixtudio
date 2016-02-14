bd835dda3140af8f3f6355124485498173068ce6
Joseba García Etxebarria <joseba.gar@gmail.com>
2016-02-14 20:39:17 +0100

    Separate font loading from rendering -> glyphs are pre-rendered when a font is loaded * Easier glyph rendering code * Accurately compute text box size

--- modules/mod_ttf/mod_ttf.c
+++ modules/mod_ttf/mod_ttf.c
@@ -50,6 +50,11 @@
 typedef struct {
     FT_Face   face;
     bool      loaded;
+    GRAPH    *gr_glyph[256];
+    uint32_t  advance_x[256];
+    uint32_t  advance_y[256];
+    uint32_t  left[256];
+    uint32_t  top[256];
 } FONTFACE;
 
 FONTFACE faces[MAX_FACES];
@@ -57,6 +62,53 @@ FONTFACE faces[MAX_FACES];
 /* ---------------------- */
 FT_Library library;
 /* ---------------------- */
+
+bool preload_glyphs(int n) {
+    // Pre-render all tha glyph graphs
+    for (uint16_t charcode = 0; charcode < 256; charcode++) {
+        // Is there a previous graph? -> unload it
+        if(faces[n].gr_glyph[charcode]) {
+            bitmap_destroy(faces[n].gr_glyph[charcode]);
+        }
+
+        // Get the font face index for CP850 character n
+        FT_UInt glyph_index = FT_Get_Char_Index(faces[n].face, cp850_to_utf8[charcode]);
+
+        // load glyph image into the slot without rendering
+        int error = FT_Load_Glyph(faces[n].face, glyph_index, FT_LOAD_TARGET_LIGHT | FT_LOAD_RENDER);
+        if (error) {
+            continue;
+        }
+
+        // Store the font metrics
+        faces[n].advance_x[charcode] = faces[n].face->glyph->advance.x >> 6;
+        faces[n].advance_y[charcode] = faces[n].face->glyph->advance.y >> 6;
+        faces[n].left[charcode]      = faces[n].face->glyph->bitmap_left;
+        faces[n].top[charcode]       = faces[n].face->glyph->bitmap_top;
+
+        // Create a 8bpp graph for the glyph image, which
+        if(faces[n].face->glyph->bitmap.width > 0 && faces[n].face->glyph->bitmap.rows > 0) {
+            faces[n].gr_glyph[charcode] = bitmap_new(charcode, faces[n].face->glyph->bitmap.width, faces[n].face->glyph->bitmap.rows, 8);
+            bitmap_add_cpoint(faces[n].gr_glyph[charcode], 0, 0);
+            // Store the contents of the glyph bitmap
+            uint8_t *graph_pos;
+            uint8_t *alpha_pos;
+
+            for(uint32_t y = 0; y < faces[n].face->glyph->bitmap.rows; y++) {
+                for(uint32_t x = 0; x < faces[n].face->glyph->bitmap.width; x++) {
+                    graph_pos = ((uint8_t *)faces[n].gr_glyph[charcode]->data) + faces[n].gr_glyph[charcode]->pitch * y + x;
+                    alpha_pos = ((uint8_t *)faces[n].face->glyph->bitmap.buffer) + faces[n].face->glyph->bitmap.pitch * y + x;
+                    *graph_pos = *alpha_pos;
+                }
+            }
+        } else {
+            // Mark the GRAPH pointer empty so that we don't use it
+            faces[n].gr_glyph[charcode] = NULL;
+        }
+    }
+
+    return true;
+}
 
 bool load_face(const char *path, uint16_t size, uint8_t n) {
     // First of all, read the whole file into memory
@@ -89,7 +141,7 @@ bool load_face(const char *path, uint16_t size, uint8_t n) {
     file_close(fp);
 
     // Create the font face and perform some basic checks
-    int error = FT_New_Memory_Face (library, (const FT_Byte*)data, read, 0, &faces[n].face);
+    int error = FT_New_Memory_Face(library, (const FT_Byte*)data, read, 0, &faces[n].face);
     if(error) {
         if(debug) {
             PXTRTM_LOGERROR("ERROR: Could not load font face %s\n", path);
@@ -114,11 +166,14 @@ bool load_face(const char *path, uint16_t size, uint8_t n) {
     }
 
     // Set the character size in px
-    error = FT_Set_Pixel_Sizes(faces[n].face, 15, 15);
+    error = FT_Set_Pixel_Sizes(faces[n].face, size, size);
     if(error) {
         FT_Done_Face(faces[n].face);
         return false;
     }
+
+    // Store the glyph bitmaps
+    preload_glyphs(n);
 
     // Mark the font face as loaded
     faces[n].loaded = true;
@@ -126,10 +181,65 @@ bool load_face(const char *path, uint16_t size, uint8_t n) {
     return true;
 }
 
+// Determine the size of a text's bounding box
+bool get_text_size(const char *text, uint8_t fontid, uint32_t *w, uint32_t *h, int32_t *baseline_y) {
+    int32_t top_pixel = 0;
+    int32_t bottom_pixel = 0;
+    *w = 0;
+    *h = 0;
+    *baseline_y = 0;
+    unsigned char code;
+    FT_UInt previous    = 0;
+    uint32_t num_glyphs = strlen(text);
+
+    // Run through all the glyphs and compute the box size
+    for (uint32_t n = 0; n < num_glyphs; n++) {
+        // Character code in CP850 encoding
+        code = (unsigned char)text[n];
+
+        // Is this glyph bigger than the biggest? -> Store the value
+        if(top_pixel < faces[fontid].top[code]) {
+            top_pixel = faces[fontid].top[code];
+        }
+
+        if(faces[fontid].gr_glyph[code] && (bottom_pixel > (int32_t)(faces[fontid].top[code] - faces[fontid].gr_glyph[code]->height))) {
+            bottom_pixel = faces[fontid].top[code] - faces[fontid].gr_glyph[code]->height;
+        }
+
+        // Is this glyph deeper than the deepest? -> Store the value
+        if(faces[fontid].gr_glyph[code] && (faces[fontid].gr_glyph[code]->height - faces[fontid].top[code]) > *baseline_y) {
+            *baseline_y = faces[fontid].gr_glyph[code]->height - faces[fontid].top[code];
+        }
+
+        FT_UInt glyph_index = FT_Get_Char_Index(faces[fontid].face, cp850_to_utf8[(unsigned char)text[n]]);
+
+        /* retrieve kerning distance and move pen position */
+        if (FT_HAS_KERNING(faces[fontid].face) && previous && glyph_index) {
+            FT_Vector  delta;
+
+            FT_Get_Kerning(faces[fontid].face, previous, glyph_index, FT_KERNING_DEFAULT, &delta);
+
+            *w += (delta.x >> 6);
+        }
+
+        /* increment pen position */
+        *w += faces[fontid].advance_x[code];
+
+        /* record current glyph index */
+        previous = glyph_index;
+    }
+
+    *w += faces[fontid].left[code] + faces[fontid].gr_glyph[code]->width;
+    *h  = top_pixel - bottom_pixel;
+
+
+    return -1;
+}
+
 int ttf_draw(INSTANCE *my, int *params) {
-    int error;
     // HACK, HACK, HAAAACK!
     extern int fntcolor32;
+    unsigned char code;
 
     if (!scr_initialized) {
         return (-1);
@@ -143,27 +253,19 @@ int ttf_draw(INSTANCE *my, int *params) {
     uint16_t pen_x = 0;
     uint16_t pen_y = 0;
 
-    FT_UInt num_glyphs  = 0;
     FT_Bool use_kerning = FT_HAS_KERNING(faces[0].face);
     FT_UInt previous    = 0;
 
     const char *text = string_get(params[1]);
-    uint32_t num_chars = strlen(text);
+    uint32_t num_glyphs = strlen(text);
 
-    FT_Glyph      glyphs[num_chars];   /* glyph image    */
-    FT_Vector     pos   [num_chars];   /* glyph position */
+    FT_Vector pos[num_glyphs];   /* glyph position */
 
-    for (uint32_t n = 0; n < num_chars; n++) {
-        /*
-         * convert character code to glyph index
-         * FreeType expects Unicode char codes by default
-         * and we'll be receiving DOS-Latin-1 (CP850) codes here,
-         * so we need a correspondance table.
-         * Wikipedia has a list of unicode character codes here:
-         * https://en.wikipedia.org/wiki/List_of_Unicode_characters
-         * (We want the "Decimal" column)
-         * https://msdn.microsoft.com/en-us/library/cc195064.aspx
-         */
+    // TODO: This can probably be moved into get_text_size
+    for (uint32_t n = 0; n < num_glyphs; n++) {
+        // Character code in CP850 encoding
+        code = (unsigned char)text[n];
+
         FT_UInt glyph_index = FT_Get_Char_Index(faces[0].face, cp850_to_utf8[(unsigned char)text[n]]);
 
         /* retrieve kerning distance and move pen position */
@@ -172,89 +274,53 @@ int ttf_draw(INSTANCE *my, int *params) {
 
             FT_Get_Kerning(faces[0].face, previous, glyph_index, FT_KERNING_DEFAULT, &delta);
 
-            pen_x += delta.x >> 6;
+            pen_x += (delta.x >> 6);
         }
 
         /* store current pen position */
-        pos[num_glyphs].x = pen_x;
-        pos[num_glyphs].y = pen_y;
-
-        /* load glyph image into the slot without rendering */
-        error = FT_Load_Glyph(faces[0].face, glyph_index, FT_LOAD_DEFAULT);
-        if (error) {
-            continue;  /* ignore errors, jump to next glyph */
-        }
-
-        /* extract glyph image and store it in our table */
-        error = FT_Get_Glyph(faces[0].face->glyph, &glyphs[num_glyphs]);
-        if (error) {
-            continue;  /* ignore errors, jump to next glyph */
-        }
+        pos[n].x = pen_x;
+        pos[n].y = pen_y;
 
         /* increment pen position */
-        pen_x += faces[0].face->glyph->advance.x >> 6;
+        pen_x += faces[0].advance_x[code];
 
         /* record current glyph index */
         previous = glyph_index;
-
-        /* increment number of glyphs */
-        num_glyphs++;
     }
 
     // Decrease reference count, now that we're done with it
     string_discard(params[1]);
 
-    // Create the graph holding the text
-    GRAPH *alpha_graph = NULL;
+    // Determine the size of the text's bounding box
+    uint32_t w, h;
+    int32_t baseline_y;
+    get_text_size(text, 0, &w, &h, &baseline_y);
 
-    // Make the box as high as needed so that any text fits in
-    uint32_t highest_y = FT_MulFix(abs(faces[0].face->ascender), faces[0].face->size->metrics.y_scale) >> 6;
-    uint32_t baseline_y = FT_MulFix(abs(faces[0].face->descender), faces[0].face->size->metrics.y_scale) >> 6;
+    // This GRAPH will hold the alpha channel for the text
+    GRAPH *alpha_graph = bitmap_new(0, w, h, 8);
+    if (!alpha_graph) {
+        if(debug) {
+            PXTRTM_LOGERROR("ERROR: Could not create alpha GRAPH\n");
+        }
+        FT_Done_Face(faces[0].face);
+        return -1;
+    }
+    gr_clear(alpha_graph);
 
     // Draw the text into the GRAPH
-    for (int16_t n = num_glyphs-1; n >= 0; n-- ) {
-        FT_Glyph   image;
+    for (int16_t n = 0; n < num_glyphs; n++) {
         FT_Vector  pen;
-
-        image = glyphs[n];
 
         pen.x = pos[n].x;
         pen.y = pos[n].y;
 
-        error = FT_Glyph_To_Bitmap(&image, FT_RENDER_MODE_NORMAL, &pen, 0);
-        if ( !error ) {
-            FT_BitmapGlyph bit = (FT_BitmapGlyph)image;
-            if( bit->bitmap.width > 0) {
-                if(alpha_graph == NULL) {
-                    alpha_graph = bitmap_new(0,
-                                             pen.x + bit->bitmap.width + bit->left,
-                                             highest_y + baseline_y,
-                                             8);
-                    if (!alpha_graph) {
-                        if(debug) {
-                            PXTRTM_LOGERROR("ERROR: Could not create alpha GRAPH\n");
-                        }
-                        FT_Done_Face(faces[0].face);
-                        return -1;
-                    }
+        // Character code in CP850 encoding
+        code = (unsigned char)text[n];
 
-                    // Clear the graph (make all the points transparent)
-                    gr_clear(alpha_graph);
-                }
-
-                // Create a 8bpp graph for the glyph
-                GRAPH *glyph_graph = bitmap_new_ex(0,
-                                                   bit->bitmap.width, bit->bitmap.rows, 8,
-                                                   bit->bitmap.buffer, bit->bitmap.pitch);
-                gr_blit(alpha_graph, NULL,
-                        pen.x + bit->left + glyph_graph->width/2.0,
-                        pen.y + alpha_graph->height - bit->top + glyph_graph->height/2.0 - baseline_y,
-                        0,  255,  255,  255, glyph_graph);
-
-                bitmap_destroy(glyph_graph);
-            }
-
-            FT_Done_Glyph( image );
+        if(faces[0].gr_glyph[code]) {
+            int x = pen.x + faces[0].left[code];
+            int y = pen.y + h - faces[0].top[code] - baseline_y;
+            gr_blit(alpha_graph, NULL, x, y, 0,  255,  255,  255, faces[0].gr_glyph[code]);
         }
     }
 
