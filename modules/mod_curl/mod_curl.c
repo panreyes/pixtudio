21b480af175143352f0186eb9f8afbb2e7fe6b17
Joseba García Etxebarria <joseba.gar@gmail.com>
2016-03-07 11:17:48 +0100

    Fixed typo in my name (?)

--- modules/mod_curl/mod_curl.c
+++ modules/mod_curl/mod_curl.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2011-2015 Joseba García Echebarria. All rights reserved.
+ *  Copyright (C) 2011-2015 Joseba García Etxebarria. All rights reserved.
  *
  *  This software is provided 'as-is', without any express or implied
  *  warranty. In no event will the authors be held liable for any damages
