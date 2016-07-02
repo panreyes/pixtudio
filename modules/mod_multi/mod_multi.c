37aa67d0ebd79a6b33779aef948eac217936e1f6
Joseba García Etxebarria <joseba.gar@gmail.com>
2016-07-02 18:18:50 +0200

    More minor work on #64

--- modules/mod_multi/mod_multi.c
+++ modules/mod_multi/mod_multi.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2016 Joseba García Etxebarria. All rights reserved.
+ * Copyright (C) 2013-2016 Joseba García Etxebarria. All rights reserved.
  *
  *  This software is provided 'as-is', without any express or implied
  *  warranty. In no event will the authors be held liable for any damages
