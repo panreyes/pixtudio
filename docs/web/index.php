e302bd5f0a0a8eef0198e15708e9ded21106ba87
Joseba Echevarria García <joseba.gar@gmail.com>
2018-12-19 00:02:42 +0100

    Removed google analytics from the web page

--- docs/web/index.php
+++ docs/web/index.php
@@ -8,37 +8,6 @@
         <meta name="author" content="Joseba García Etxebarria"/>
         <link rel="stylesheet" href="main.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <script>
-            /* Google analytics */
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', 'UA-72167122-1', 'auto');
-            ga('send', 'pageview');
-
-            /* Link scroll */
-            $(document).ready(function () {
-                $('a[href^="#"]').on('click', function (e) {
-                    e.preventDefault();
-
-                    var target = this.hash,
-                        $target = $(target);
-
-                    $('html, body').stop().animate({
-                        'scrollTop': $target.offset().top - 80
-                    }, 300, 'swing');
-                });
-            });
-        </script>
     </head>
     <body>
     <ul class="nav">
